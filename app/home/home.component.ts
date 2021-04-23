ee488fa3c9de8992ca839e890f3d581cf75f1da9
Vijay <vijay@themounthead.com>
2021-04-23 13:32:23 +0530

    initial commit

---
+++ app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  title = 'nativescript-ngx-partials';
+  private counter = 42;
+
+  constructor() { }
+
+  public getMessage() {
+    return this.counter > 0 ?
+      `${this.counter} taps left` :
+      'Hoorraaay! You unlocked the NativeScript clicker achievement!';
+  }
+
+  public onTap() {
+    this.counter--;
+  }
+}
